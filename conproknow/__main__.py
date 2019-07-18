8a0dfa0e83661d558f07641c4c0e3414eb67d072
PHParis <pierrehenri.paris@gmail.com>
2019-07-18 17:49:46 +0200

    Work in progress: parameters handling

--- conproknow/__main__.py
+++ conproknow/__main__.py
@@ -1,5 +1,7 @@
+import argparse
 from getopt import getopt, GetoptError
 from sys import argv, exit
+from os.path import isfile
 from conproknow.identity.lattice import Lattice
 from conproknow.kg.knowledge_graph import KG
 from conproknow.kg.hdt_knowledge_graph import HDT
@@ -12,9 +14,32 @@ def usage():
     print("Type -o or --output to specify the output directory. Default is /data2/hamdif/doctorants/ph/xp/")
     print("Type -r or --resource to specify the resource from which the identity context will be computed. Default is http://dbpedia.org/resource/France")
     print("Type -f or --file to specify the HDT file to process. Default is /data2/hamdif/doctorants/ph/linkeddatasets/hdt/lod-a-lot/LOD_a_lot_v1.hdt")
+    print("Type -gs or --gold_standard to test the gold standard. Default is /data2/hamdif/doctorants/ph/linkeddatasets/hdt/lod-a-lot/LOD_a_lot_v1.hdt")
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group1 = parser.add_mutually_exclusive_group() #.add_mutually_exclusive_group(required=True)
+    group2 = group1.add_argument_group("lattice")
+    group2.add_argument("resource")
+    group2.add_argument("lattice")
+    group1.add_argument("--gold_standard", help="Valid gold standard file path.")    
+    # group2 = parser.add_argument_group() #.add_mutually_exclusive_group(required=True)
+    # group2.add_argument("lattice2")
+    # group2.add_argument("gold_standard2")
+    args = parser.parse_args()
+    print(args)
+    exit(0)
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
     try:
         opts, args = getopt(argv[1:], "ho:r:f:", [
             "help", "output=", "resource=", "file="])
@@ -33,7 +58,8 @@ if __name__ == "__main__":
     resource = "http://dbpedia.org/resource/Paris"
     resource = "http://dbpedia.org/resource/France"
     output_dir = "/data2/hamdif/doctorants/ph/xp/"
-
+    gold_standard_path = "gold_standard/gold_standard.json"
+    print("t1")
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
@@ -44,8 +70,11 @@ if __name__ == "__main__":
             resource = a
         elif o in ("-f", "--file"):
             hdt_file_path = a
+        elif o in ("-gs", "--gold_standard"):
+            gold_standard_path = a
         else:
             assert False, "unhandled option"
+    exit(0)
     kg: KG = HDT(hdt_file_path)
     lattice = build_lattice(
         "http://www.wikidata.org/entity/Q2343504", kg, None, False)
